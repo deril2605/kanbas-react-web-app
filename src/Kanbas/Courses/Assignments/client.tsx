d73d09be36f148abd8a97377cfb585db96264a26
deril2605 <derilraju@gmail.com>
2024-03-26 17:14:14 -0400

    assgn vars

--- src/Kanbas/Courses/Assignments/client.tsx
+++ src/Kanbas/Courses/Assignments/client.tsx
@@ -1,8 +1,12 @@
 import axios from "axios";
-const COURSES_API = "http://localhost:4000/api/courses";
+// const COURSES_API = "http://localhost:4000/api/courses";
 // const COURSES_API = "https://kanbas-node-server-app-2kns.onrender.com/api/courses";
-const ASSIGNMENTS_API = "http://localhost:4000/api/assignments";
+// const ASSIGNMENTS_API = "http://localhost:4000/api/assignments";
 // const ASSIGNMENTS_API = "https://kanbas-node-server-app-2kns.onrender.com/api/assignments";
+
+const API_BASE = process.env.REACT_APP_API_BASE;
+const COURSES_API = `${API_BASE}/api/courses`;
+const ASSIGNMENTS_API = `${API_BASE}/api/assignments`;
 
 interface Assignment {
     _id: string;
